3df2ada9f87ad6b2ef428477e74c51aa078e0a80
Bogdan Kartavenko <bogdankartavenko@gmail.com>
2020-12-07 15:13:23 +0200

    task10 fix

--- JS1/task10.js
+++ JS1/task10.js
@@ -2,4 +2,4 @@ let year = +prompt("Укажите год");
 let month = +prompt("Укажите месяц");
 let day = +prompt("Укажите день");
 let date = new Date((year),(month -1),(day +1));
-alert(date.toLocaleString("ua") );
+alert(date.toLocaleDateString());
